fa43b5f4c183ec3766d001f89bed06337a2a0ba9
aptivator <aptivator@gmail.com>
2017-09-05 13:40:57 -0400

    Adjust http wrappers method

--- src/http-wrappers/http-wrappers.ts
+++ src/http-wrappers/http-wrappers.ts
@@ -3,10 +3,12 @@ import httpClientMethodNames from './_lib/http-client-method-names';
 import requestPreprocessor   from './request-preprocessor/request-preprocessor'; 
 
 export default _.reduce(httpClientMethodNames, (proto, methodNames, methodType) => {
+  let queryMethod = methodType === 'query';
+  
   return _.reduce(methodNames, (proto, methodName) => {
     return _.extend(proto, {
       [methodName]: function(url, body, options) {
-        let args = requestPreprocessor.call(this, url, body, options, methodType);
+        let args = requestPreprocessor.call(this, url, body, options, queryMethod);
         return this.http[methodName](...args);
       }
     });
