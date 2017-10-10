169726283a85772116610f85525cdf7cfc14f197
aptivator <aptivator@gmail.com>
2017-10-10 13:25:00 -0400

    Add explicit properties on the constructor

--- dist/_constructor/constructor.d.ts
+++ dist/_constructor/constructor.d.ts
@@ -1,5 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 export declare class HttpClientPlus {
     private http;
+    init: any;
+    getToken: any;
+    setToken: any;
+    removeToken: any;
+    get: any;
+    post: any;
+    put: any;
+    patch: any;
+    delete: any;
     constructor(http: HttpClient);
 }
--- src/_constructor/constructor.ts
+++ src/_constructor/constructor.ts
@@ -3,5 +3,15 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class HttpClientPlus {
+  public init;
+  public getToken;
+  public setToken;
+  public removeToken;
+  public get;
+  public post;
+  public put;
+  public patch;
+  public delete;
+  
   constructor(private http: HttpClient) {}
 }
